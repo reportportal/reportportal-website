a90e1f0ab2473d61d649af2982808a4debde4cc3
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2025-10-29 15:48:23 +0400

    EPMRPP-108972 || EPMRPP-108796 || build fix

--- src/containers/ContactUsPage/ContactUsForm/constants.ts
+++ src/containers/ContactUsPage/ContactUsForm/constants.ts
@@ -1,6 +1,8 @@
-export const MAX_LENGTH = 255;
-export const RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY;
+const siteKey = process.env.RECAPTCHA_SITE_KEY;
 
-if (!RECAPTCHA_SITE_KEY) {
+if (!siteKey) {
   throw new Error('RECAPTCHA_SITE_KEY environment variable is not set');
 }
+
+export const MAX_LENGTH = 255;
+export const RECAPTCHA_SITE_KEY = String(siteKey);
