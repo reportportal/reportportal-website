12676c4d017e2baa6aa165e61f4fc28862c2d7a4
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2025-10-29 15:40:56 +0400

    EPMRPP-108972 || EPMRPP-108796 || coderabbitai crf

--- src/containers/ContactUsPage/ContactUsForm/constants.ts
+++ src/containers/ContactUsPage/ContactUsForm/constants.ts
@@ -1,2 +1,6 @@
 export const MAX_LENGTH = 255;
-export const RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY as string;
+export const RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY;
+
+if (!RECAPTCHA_SITE_KEY) {
+  throw new Error('RECAPTCHA_SITE_KEY environment variable is not set');
+}
