eb6bccd3f5134a71ee9decea3f29ebee29a589de
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2026-03-16 06:37:59 +0400

    EPMRPP-113467 || Add Organization JSON-LD schema

---
+++ src/components/StructuredData/constants.ts
@@ -0,0 +1,10 @@
+export const SITE_URL = 'https://reportportal.io';
+export const SITE_NAME = 'ReportPortal';
+export const LOGO_URL = `${SITE_URL}/logo/logo-build.svg`;
+
+export const SOCIAL_LINKS = [
+  'https://github.com/reportportal',
+  'https://twitter.com/ReportPortal_io',
+  'https://www.linkedin.com/company/reportportal/',
+  'https://www.youtube.com/@ReportPortal',
+];
