3e92cad06f0c88fa4b9079fdd45879ae0c0e5a0e
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2026-03-16 13:43:02 +0400

    EPMRPP-113467 || Update Twitter social link to X

--- src/components/StructuredData/constants.ts
+++ src/components/StructuredData/constants.ts
@@ -4,7 +4,7 @@ export const LOGO_URL = `${SITE_URL}/logo/logo-build.svg`;
 
 export const SOCIAL_LINKS = [
   'https://github.com/reportportal',
-  'https://twitter.com/ReportPortal_io',
+  'https://x.com/ReportPortal_io',
   'https://www.linkedin.com/company/reportportal/',
   'https://www.youtube.com/@ReportPortal',
 ];
