ab3d65ad05a18aec3663c0ebe6f88664430d1ccc
Viktor Soroka <viktor_soroka@epam.com>
2026-04-18 02:24:06 +0300

    fix: hide decorative search icon from assistive tech

--- src/svg/searchIcon.tsx
+++ src/svg/searchIcon.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 export const SearchIcon = () => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      focusable="false"
+    >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
